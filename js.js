7d6268f3af1076d7f424ec4eca5acce78ba5506b
Sonal Suralikal <115199260+demisun12@users.noreply.github.com>
2024-05-13 10:56:19 -0400

    Write JS for button
    Add button prompt to set new grid dimensions

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector("#grid");
 
+const setgrid = document.querySelector("#number");
+
 for (let col = 0; col < 16; col++) {
     const gridRow = document.createElement("div");
     gridRow.setAttribute("class", "grid-row");
@@ -9,5 +11,22 @@ for (let col = 0; col < 16; col++) {
         gridRowDivs.setAttribute("class", "square");
         gridRow.appendChild(gridRowDivs);
 
+    }
+}
+
+ setgrid.onclick = function () {
+    let choice = prompt("Enter the number of squares per side of grid");
+    let gridNumber = parseInt(choice);
+    grid.innerHTML = "";
+    for (let col = 0; col < gridNumber; col++) {
+        const gridRow = document.createElement("div");
+        gridRow.setAttribute("class", "grid-row");
+        grid.appendChild(gridRow);
+        for (let row = 0; row < gridNumber; row++) {
+            const gridRowDivs = document.createElement("div");
+            gridRowDivs.setAttribute("class", "square");
+            gridRow.appendChild(gridRowDivs);
+    
+        }
     }
 }
